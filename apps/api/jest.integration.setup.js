97f91d5a547bd1a8d05155f4113bbffa649bdabc
Maxime <maxime.lecourt457@gmail.com>
2026-06-24 14:10:50 +0200

    chore(api): configure Jest for unit and integration tests

---
+++ apps/api/jest.integration.setup.js
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: ".env.test" })
+
+const mongoose = require("mongoose")
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGODB_URI)
+})
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase()
+  await mongoose.disconnect()
+})
